c72e490c8194fed321b8e8d1b344adfd9b3cd111
Dan Lysiak <danlysiak@hotmail.com>
2026-01-17 16:51:28 +0000

    Use `sslmode=verify-full`

--- src/main/modules/analytics/shared/data/prisma.ts
+++ src/main/modules/analytics/shared/data/prisma.ts
@@ -25,6 +25,7 @@ function buildDatabaseUrlFromConfig(prefix: string): string | undefined {
     return undefined;
   }
 
+  console.log(`Authenticating as user ${user} to database ${database}, schema ${schema}`);
   const auth = password ? `${encodeURIComponent(user)}:${encodeURIComponent(password)}` : encodeURIComponent(user);
   const optionsParams = [];
   if (options) {
