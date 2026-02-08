ff242cd3bdfb28d11ea7fa03eed8a6509dd5e0bd
Dan Lysiak <danlysiak@hotmail.com>
2026-02-08 11:39:21 +0000

    Fix flaky tests; right-align sign out button

--- jest.routes.config.js
+++ jest.routes.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   roots: ['<rootDir>/src/test/routes'],
   testRegex: '(/src/test/.*|\\.(test|spec))\\.(ts|js)$',
-  testPathIgnorePatterns: ['<rootDir>/src/test/routes/routeTestUtils.ts'],
+  testPathIgnorePatterns: ['<rootDir>/src/test/routes/routeTestUtils.ts', '<rootDir>/src/test/routes/setup.ts'],
+  clearMocks: true,
+  resetModules: true,
+  setupFiles: ['<rootDir>/src/test/routes/setup.ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
   testEnvironment: 'node',
   testTimeout: 30000,
